25d27be518cba5f3ce53d4433af5ed4ff68a1537
Pankaj Rawat <pankaj.rawat01@nagarro.com>
2025-02-11 19:22:12 +0530

    Updated env variables with API key

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyDK4L9AyfWpkFeXgewBhlKaNXukeyma9WM";
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 export const YOUTUBE_VIDEOS_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" + GOOGLE_API_KEY;
 
 export const SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
